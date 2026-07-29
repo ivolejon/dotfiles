987bc1e2db5fdf04ba596e0f3c69e6ef1300342f
Ivo Lejon <ivo.lejon@gmail.com>
2026-07-29 19:48:05 +0200

    update

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -6,4 +6,17 @@
 
   system.primaryUser = "ivolej01";
   system.stateVersion = 6;
+  system.defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        KeyRepeat = 2;          # fast key repeat
+        InitialKeyRepeat = 15;  # short delay before repeat
+        _HIHideMenuBar = true;  # auto-hide the menu bar
+        AppleShowAllExtensions = true;
+      };
+      dock.autohide = true;
+      finder.FXPreferredViewStyle = "Nlsv";  # list view by default
+      finder.CreateDesktop = false;          # clean desktop
+      trackpad.Clicking = true;              # tap to click
+    };
 }
